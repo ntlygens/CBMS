66be8335da29d918d44a7c22970ca8d2681457f4
Alden Willis <ntlygens@gmail.com>
2017-10-24 19:13:13 -0400

    Second Commit Works

--- client/main.ts
+++ client/main.ts
@@ -1,4 +1,5 @@
 import "angular2-meteor-polyfills";
+import { Meteor } from "meteor/meteor";
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { enableProdMode } from "@angular/core";
