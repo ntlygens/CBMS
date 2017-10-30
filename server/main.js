0bb5aa61009e69762f2b5b792ba28e50b1e5872d
Alden Willis <ntlygens@gmail.com>
2017-10-29 21:27:31 -0400

    Ver. 0.0.1 Updated package.json meteor running

--- server/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var main_1 = require("./server-main/main");
-var mainApp = new main_1.InitData();
-mainApp.start();
-/*Meteor.startup(() => {
-  // code to run on server at startup
-});*/
